ae954507669b6a0b72e6e868c0e92eacd56893be
henrysteiner <34252893+henrysteiner@users.noreply.github.com>
2018-03-16 19:46:31 -0700

    Add files via upload

--- edgeDetect.m
+++ edgeDetect.m
@@ -64,100 +64,100 @@ for i=1:size(adjSum,1)
     end
 end
 
-%% Voronoi Attempt 1: Traverse through all Possible Paths using Stack
-
-visited = zeros(size(imArr));
-points = [];
-stack = [1,1];
-
-while(~isempty(stack))
-    
-    % Pop off the stack
-    cur = stack(end,:)
-    stack = stack(1:end-1, :);
-    
-    % Add it to the path of points we have visited
-    points = [points; cur];
-    
-    % Base case, In this case do nothing and let this iteration complete
-    if((imArr(cur(1), cur(2)) == 1 && (cur(1)~=1||cur(2)~=1)))
-        disp('base case')
-        visited(cur(1), cur(2)) = 1;
-    % If not the base case, see what other points we should explore.
-    else
-        % Get the max value surrounding as well as the cells that
-        % contain that value
-        [max, xMax, yMax] = getAdjRel(imArr, cur, visited, 1);
-        xList = [];
-        yList = [];
-        
-        % If that maximum value is larger or equal than the current cell value
-        if max >= imArr(cur(1), cur(2))
-            if length(xMax) > 1
-                
-                [~, adjX, adjY] = getAdjRel(adjSum, cur, visited, 1);
-
-                for i=1:length(adjX)
-                    for j=1:length(xMax)
-                        if adjX(i) == xMax(j) && adjY(i) == yMax(j)
-                            xList = [xList;xMax(j)];
-                            yList = [yList;yMax(j)];
-                        end
-                    end
-                end
-            else
-                xList = xMax;
-                yList = yMax;
-            end
-        end
-
-        % If the max value is not larger, it means we need to trace a line
-        % back to the edge. We do this by looking at the unvisited adjacent cell that
-        % has the minimum adjacent cell sum.
-        if isempty(xList)
-            [~, xMin, yMin] = getAdjRel(adjSum, cur, visited, 0);
-            xList = xMin;
-            yList = yMin;
-        end
-
-        % Since we have chosen not to visit the rest of them, we can mark
-        % the remaining cells as visited. 
-        % visited = markAdditional(xList,yList,visited, cur(1), cur(2));
-        
-        % If only one possible cell to go to based on x/y list
-        if length(xList) == 1
-            newPoint = [xList(1),yList(1)];
-            stack = [stack;newPoint];
-        % If a split is necessary
-        elseif length(xList) > 1
-            % For every new point to go to we add it to the stack
-            for i=1:length(xList)
-                disp("needs to split");
-                newPoint = [xList(i), yList(i)];
-                % The function is called on the next coordinates to go
-                % to.
-                stack = [stack; newPoint];
-            end
-        else
-            disp('no possible found')
-        end
-    end 
-    
-    % Since we have found all the values to explore, we can mark this as
-    % visited.
-    visited(cur(1), cur(2)) = 1;
-    
-end
+% %% Voronoi Attempt 1: Traverse through all Possible Paths using Stack
+% 
+% visited = zeros(size(imArr));
+% points = [];
+% stack = [1,1];
+% 
+% while(~isempty(stack))
+%     
+%     % Pop off the stack
+%     cur = stack(end,:)
+%     stack = stack(1:end-1, :);
+%     
+%     % Add it to the path of points we have visited
+%     points = [points; cur];
+%     
+%     % Base case, In this case do nothing and let this iteration complete
+%     if((imArr(cur(1), cur(2)) == 1 && (cur(1)~=1||cur(2)~=1)))
+%         disp('base case')
+%         visited(cur(1), cur(2)) = 1;
+%     % If not the base case, see what other points we should explore.
+%     else
+%         % Get the max value surrounding as well as the cells that
+%         % contain that value
+%         [max, xMax, yMax] = getAdjRel(imArr, cur, visited, 1);
+%         xList = [];
+%         yList = [];
+%         
+%         % If that maximum value is larger or equal than the current cell value
+%         if max >= imArr(cur(1), cur(2))
+%             if length(xMax) > 1
+%                 
+%                 [~, adjX, adjY] = getAdjRel(adjSum, cur, visited, 1);
+% 
+%                 for i=1:length(adjX)
+%                     for j=1:length(xMax)
+%                         if adjX(i) == xMax(j) && adjY(i) == yMax(j)
+%                             xList = [xList;xMax(j)];
+%                             yList = [yList;yMax(j)];
+%                         end
+%                     end
+%                 end
+%             else
+%                 xList = xMax;
+%                 yList = yMax;
+%             end
+%         end
+% 
+%         % If the max value is not larger, it means we need to trace a line
+%         % back to the edge. We do this by looking at the unvisited adjacent cell that
+%         % has the minimum adjacent cell sum.
+%         if isempty(xList)
+%             [~, xMin, yMin] = getAdjRel(adjSum, cur, visited, 0);
+%             xList = xMin;
+%             yList = yMin;
+%         end
+% 
+%         % Since we have chosen not to visit the rest of them, we can mark
+%         % the remaining cells as visited. 
+%         % visited = markAdditional(xList,yList,visited, cur(1), cur(2));
+%         
+%         % If only one possible cell to go to based on x/y list
+%         if length(xList) == 1
+%             newPoint = [xList(1),yList(1)];
+%             stack = [stack;newPoint];
+%         % If a split is necessary
+%         elseif length(xList) > 1
+%             % For every new point to go to we add it to the stack
+%             for i=1:length(xList)
+%                 disp("needs to split");
+%                 newPoint = [xList(i), yList(i)];
+%                 % The function is called on the next coordinates to go
+%                 % to.
+%                 stack = [stack; newPoint];
+%             end
+%         else
+%             disp('no possible found')
+%         end
+%     end 
+%     
+%     % Since we have found all the values to explore, we can mark this as
+%     % visited.
+%     visited(cur(1), cur(2)) = 1;
+%     
+% end
 
 %% Voronoi Attempt2: Find Midpoints between Obstacles and Draw Path
 
-points = []
+v_points = [];
 for i=1:size(imArr,1)
     for j=1:size(imArr,2)
         cur = [i,j];
         if checkCell(imArr, cur)
             cur
-            points = [points; cur];
+            v_points = [v_points; cur];
         end
     end
 end
@@ -166,10 +166,37 @@ end
 
 imshow(imresize(I,0.5));
 hold on
-scatter(points(:,2),points(:,1), 1, 'red', 'filled');
+scatter(v_points(:,2),v_points(:,1), 5, 'red','square', 'filled');
+hold on
 
-%% Voronoi Built In Attempt
+% %% Voronoi Built In Attempt
+% 
+% % Attempt using built in voronoi function
+% [r, c] = find(tempArr2);
+% voronoi(r,c)
 
-% Attempt using built in voronoi function
-[r, c] = find(tempArr2);
-voronoi(r,c)
+
+% Use data cursor to select robot point and export point as "robot" and "goal" to workspace
+disp('Choose [robot] starting location')
+pause;
+
+xr = robot.Position(1);
+yr = robot.Position(2);
+
+xg = goal.Position(1);
+yg = goal.Position(2);   
+
+scatter(xr,yr,200,'y','filled')
+hold on
+scatter(xg,yg,200,'g','filled')
+hold on
+
+[closestx,closesty] = findClosest(xr,yr,v_points);
+plot([xr closestx],[yr closesty],'LineWidth',1)
+hold on
+[closestx,closesty] = findClosest(xg,yg,v_points);
+plot([xg,closestx],[yg,closesty],'LineWidth',1)
+
+
+
+
--- findClosest.m
+++ findClosest.m
@@ -1,25 +1,18 @@
-function line = findClosest(posX, posY, voronoi) 
-[rows,cols]=size(voronoi);
+function [closeX,closeY] = findClosest(posX, posY, v_points) 
 
+vy = v_points(:,1);
+vx = v_points(:,2);
 
-minDistance = 10000;
+minDistance = 999999999;
 closeX = 0;
 closeY = 0;
-for i=1:rows
-    pathX = voronoi(i,1);
-    pathY = voronoi(i,2);
-    
-    distance = sqrt((pathX-posX)^2+(pathY-posY)^2);
-    if distance < minDistance
-        minDistance = distance;
-        closeX = pathX;
-        closeY = pathY;
-    end
+for i=1:length(vx)
+     distance = sqrt((vx(i)-posX)^2+(vy(i)-posY)^2);
+     if distance < minDistance
+         minDistance = distance;
+         closeX = vx(i);
+         closeY = vy(i);
+     end
 end
-
-line = plot(posX,closeX,posY,closeY);
-
-
-
 end
 
